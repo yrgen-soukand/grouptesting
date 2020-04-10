a32afc9c83091a2d152996144551c6e816e83da0
Martin Porkon <martin.porkon@gmail.com>
2020-04-10 14:02:33 +0300

    first commit
    hello

--- Excercise5ReadingAndWritingSameTime/Program.cs
+++ Excercise5ReadingAndWritingSameTime/Program.cs
@@ -7,8 +7,6 @@ namespace Excercise5ReadingAndWritingSameTime
     {
         static void Main(string[] args)
         {
-            /*List<string> questionsList = new List<string>();
-            List<string> answerList = new List<string>();*/
             string line;
             int questionNr = 1;
 
@@ -33,22 +31,6 @@ namespace Excercise5ReadingAndWritingSameTime
             Console.WriteLine("Thank you for answering.");
             Console.ReadLine();
 
-            /*foreach (string question in questionsList)
-            {
-                Console.WriteLine(question);
-                answerList.Add(Console.ReadLine());
-            }
-
-            using (StreamWriter writer = new StreamWriter("sortedList.txt", true))
-            {
-                foreach (string answer in answerList)
-                { 
-                        writer.WriteLine(answer);
-                }
-
-
-            }*/
-         
         }
     }
 }
